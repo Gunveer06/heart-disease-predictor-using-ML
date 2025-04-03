09c81749291ca6b77b3f936fb3449b399820ec69
Gunveer06 <oberai.gunveer2006@gmail.com>
2025-04-03 16:52:53 +0530

    login frontend

--- frontend/heart-disease-app/src/App.js
+++ frontend/heart-disease-app/src/App.js
@@ -3,15 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import HeartDiseasePredictor from "./components/HeartDiseasePredictor";
 import ResultPage from "./components/ResultPage";
-import "./App.css"
+import LoginPage from "./components/LoginPage"; // Import LoginPage
+import "./App.css";
 
 const App = () => {
     return (
         <Router>
             <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/login" element={<LoginPage />} />  {/* New Login Route */}
                 <Route path="/predict" element={<HeartDiseasePredictor />} />
                 <Route path="/result" element={<ResultPage />} />
-                <Route path="/" element={<LandingPage/>}/>
             </Routes>
         </Router>
     );
